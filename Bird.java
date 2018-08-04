cd5778b1f7631bcb359f0f4a7802c8fc8ee1654a
zfpsusan <42081321+zfpsusan@users.noreply.github.com>
2018-08-04 00:25:38 -0700

    init code

---
+++ Bird.java
@@ -0,0 +1,7 @@
+package codeAssignment;
+
+public class Bird extends Animal{
+	void fly() {
+		System.out.println("I am flying");
+	}
+}
---
+++ Solution.java
@@ -0,0 +1,10 @@
+package codeAssignment;
+
+public class Solution {
+	public static void main (String[] args) {
+		Bird bird = new Bird();
+		bird.walk();
+		bird.fly();
+		bird.sing();
+	}
+}
