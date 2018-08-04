7056a1ef7b74a5f92f164c4db5d96ad17784a745
zfpsusan <42081321+zfpsusan@users.noreply.github.com>
2018-08-04 02:05:13 -0700

    model a fish

---
+++ Fish.java
@@ -0,0 +1,16 @@
+package codeAssignment;
+
+public class Fish extends Animal{
+	void walk() {
+		System.out.println("I can not walk");
+	}
+	
+	void sing() {
+		System.out.println("I can not sing");
+	}
+	
+	void swim() {
+		System.out.println("I am swimming");
+	}
+
+}
