360badf2d4d08b3396ef0e5bc6369995b519709c
zfpsusan <42081321+zfpsusan@users.noreply.github.com>
2018-08-04 01:45:01 -0700

    model a rooster

--- Chicken.java
+++ Chicken.java
@@ -1,8 +1,32 @@
 package codeAssignment;
 
 public class Chicken extends Bird{
+	String sex = null;
+	boolean isMature = false;
+	
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+	public boolean isMature() {
+		return isMature;
+	}
+
+	public void setMature(boolean isMature) {
+		this.isMature = isMature;
+	}
+
 	void sing() {
-		System.out.println("Cluck, cluck");
+		if ("male".equalsIgnoreCase(sex) &&
+				isMature) {
+			System.out.println("“Cock-a-doodle-doo");
+		} else {
+			System.out.println("Cluck, cluck");
+		}		
 	}
 	
 	void fly() {
--- Solution.java
+++ Solution.java
@@ -2,9 +2,35 @@ package codeAssignment;
 
 public class Solution {
 	public static void main (String[] args) {
+		System.out.println("bird");
 		Bird bird = new Bird();
 		bird.walk();
 		bird.fly();
 		bird.sing();
+		System.out.println();
+		
+		System.out.println("duck");
+		Duck duck = new Duck();
+		duck.walk();
+		duck.fly();
+		duck.sing();
+		duck.swim();
+		System.out.println();
+		
+		System.out.println("chicken");
+		Chicken chicken = new Chicken();
+		chicken.walk();
+		chicken.fly();
+		chicken.sing();
+		System.out.println();
+
+		System.out.println("rooster");
+		Chicken rooster = new Chicken();
+		rooster.setSex("male");
+		rooster.setMature(true);
+		rooster.walk();
+		rooster.fly();
+		rooster.sing();
+		System.out.println();
 	}
 }
