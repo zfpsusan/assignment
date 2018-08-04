575015629ce623e77f9f73519ba235a5ccfe6a14
zfpsusan <42081321+zfpsusan@users.noreply.github.com>
2018-08-04 01:26:07 -0700

    2 special kinds of birds: the Duck and the Chicken

---
+++ Chicken.java
@@ -0,0 +1,12 @@
+package codeAssignment;
+
+public class Chicken extends Bird{
+	void sing() {
+		System.out.println("Cluck, cluck");
+	}
+	
+	void fly() {
+		System.out.println("I can not fly");
+	}
+
+}
---
+++ Duck.java
@@ -0,0 +1,11 @@
+package codeAssignment;
+
+public class Duck extends Bird{
+	void sing() {
+		System.out.println("Quack, quack");
+	}
+	
+	void swim() {
+		System.out.println("I am swimming");
+	}
+}
